b117e0f0447f8c89ad75cc10ac32c870e3cb167a
bayo <abdulwahababdullahi064@gmail.com>
2023-06-04 08:50:23 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const preloader = document.querySelector("[data-preloader]");
+
+
+window.addEventListener("load", () => {
+preloader.classList.add("remove");
+});
+
+/**
+ * add event on mutiple element
+ */
+
+const addEventOnElements = function(elements, eventType, callback){
+    for(let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType,callback);
+    }
+}
+
+
+/**
+ * navbar toodler for mobile
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers =document.querySelectorAll("[data-nav-toggler]");
+const overlay =document.querySelector("[data-overlay]");
+
+
+
+const toggleNav = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers,"click", toggleNav );
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header");
+
+window.addEventListener("scroll", function () {
+    header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+});
